790da0be03a8bcbacce58847ab3146501c377cf6
nirvanafan205 <nirvanafan205@gmail.com>
2025-05-16 23:45:39 -0700

    project 3

---
+++ comment.java
@@ -0,0 +1,19 @@
+public class comment
+{
+	public static void main(String[] args)
+	{
+		/*
+			Hello Professor,
+
+				This is Zareen Atia Supti and my team member was Nisarg Madane.
+
+				We worked on each of the problems wtogether  and came out with conclusions.
+
+				He was a helpful team member and helped me specifically with Dijstra's algorithm to find the shortest path
+
+				I did the update distance and Nisarg did the exploring of neighboring nodes
+
+				He communicated throughly about his prgress while being reliable and keeping with his part in this group project
+		*/
+	}
+}
