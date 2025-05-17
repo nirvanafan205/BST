878bcb6900a66ab4185294b66bfdfaf1019e49ec
nirvanafan205 <nirvanafan205@gmail.com>
2025-05-16 23:51:07 -0700

    project 4

---
+++ comment2.java
@@ -0,0 +1,14 @@
+public class comment2
+{
+	public static void main(String[] args)
+	{
+		/*
+		 	Group memers: Nisarg Madane, Zareen Atia Supti
+
+			Both group members worked on extra credit taks
+			and came to conclusions. All work submitted by Nisarg Madane 
+			
+			We were both cooperative and it was fun learning from each other
+		*/
+	}
+}
